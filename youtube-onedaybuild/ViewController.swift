0ca41957e11e9493f8fc6cd384900577fcc763b2
Glen Hobby <glen.hobby@visionaustralia.org>
2020-07-29 14:37:51 +1000

    Initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Glen Hobby on 29/7/20.
+//  Copyright © 2020 Glen Hobby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
